13b44bcd29f39d5130e6c85256e6e1c9c326308e
nakayesaa <hajendraherlambang742@gmail.com>
2025-10-25 23:15:12 +0700

    another a little bit of that

--- Backend/src/prismaClient.ts
+++ Backend/src/prismaClient.ts
@@ -1,5 +1,4 @@
-
-import { PrismaClient } from "./generated/prisma/index";
+import { PrismaClient } from "@prisma/client";
 
 const prismaClient = new PrismaClient({
     log:["query", "info", "error", "warn"]
