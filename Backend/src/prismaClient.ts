5dccc83608a2b40d55b671e8099d9b1038666a76
nakayesaa <hajendraherlambang742@gmail.com>
2025-11-23 03:10:47 +0700

    adding new prisma postgre driver

--- Backend/src/prismaClient.ts
+++ Backend/src/prismaClient.ts
@@ -1,6 +1,13 @@
 import { PrismaClient } from "@prisma/client";
+import dotenv from "dotenv";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+dotenv.config();
+const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaPg({ connectionString });
 
 const prismaClient = new PrismaClient({
   log: ["query", "info", "error", "warn"],
+  adapter,
 });
 export default prismaClient;
